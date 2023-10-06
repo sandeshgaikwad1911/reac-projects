7dc882bd704fa1615f43bc5bd9de38469e4f4e15
sandesh gaikwad <sandesh.gaikwad1911@gmail.com>
2023-10-06 19:12:35 +0530

    movieX carousel

--- movieX/src/components/carousel/Carousel.jsx
+++ movieX/src/components/carousel/Carousel.jsx
@@ -1,12 +1,68 @@
-import './style.scss'
-// eslint-disable-next-line react/prop-types
-const Carousel = ({data, loading}) => {
-  console.log('CarouselData', data);
-  return (
-    <div>
-      Carousel
-    </div>
-  )
-}
+/* eslint-disable react/prop-types */
 
-export default Carousel
+import "./style.scss";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import {BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill,} from "react-icons/bs";
+import { useSelector } from "react-redux";
+import PosterFallbackImg from '../../images/no-poster.png'; 
+import LazyImg from "../lazyLoadImages/LazyImg"
+import CircleRating from '../circleRating/CircleRating'
+
+// eslint-disable-next-line react/prop-types
+const Carousel = ({ data, loading }) => {
+  console.log("CarouselData", data);
+
+  const navigation = ()=>{
+
+  }
+
+  const {url} = useSelector((state)=>state.home);
+
+  
+  return (
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {
+          !loading ? (
+            <div className="carouselItems">
+              {
+                data?.map((item)=>{
+                  const posterUrl = item?.poster_path ? url.poster + item?.poster_path : PosterFallbackImg;
+                  return(
+                    <div className="carouselItem" key={item?.id}>
+                      <div className="posterBlock">
+                        <LazyImg src={posterUrl}/>
+                        <CircleRating rating={item?.vote_average.toFixed(1)}/>
+                      </div>
+                      <div className="textBlock">
+                        <span className="title">
+                          {item?.title || item?.name}
+                          {/* for movies we have item?.title but for tv we have item?.name from api*/}
+                        </span>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          ) : (
+            <span>
+              Loading...
+            </span>
+          )
+        }
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Carousel;
--- movieX/src/components/circleRating/CircleRating.jsx
+++ movieX/src/components/circleRating/CircleRating.jsx
@@ -1,9 +1,20 @@
-import './style.scss'
-
-const CircleRating = () => {
+import "./style.scss";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+// eslint-disable-next-line react/prop-types
+const CircleRating = ({ rating }) => {
   return (
-    <div>CircleRating</div>
-  )
-}
+    <div className="circleRating">
+      <CircularProgressbar
+        value={rating}
+        maxValue={10}
+        text={rating}
+        styles={buildStyles({
+          pathColor: rating < 5 ? "red" : rating < 7 ? "orange" : "green",
+        })}
+      />
+    </div>
+  );
+};
 
-export default CircleRating
+export default CircleRating;
